5667ef0c838ffc39cd409c86af0aa157cbd15059
vrushti-mody <vrushtimody6@gmail.com>
2021-09-24 22:01:38 -0400

    Required before sharing staging link with modelers pt 1

--- components/App.vue
+++ components/App.vue
@@ -6,8 +6,8 @@
     <img src="https://covid19forecasthub.org/images/forecast-hub-logo_DARKBLUE.png"  height="30" alt="">
   </a>
 </nav>
-    <div id="vizualizations" class="container-fluid">
-    <div class = "row">
+<div id="vizualizations" class="container-fluid">
+  <div class = "row">
     <div id="options" class="col-md-3">
       <form>
         <label for = "target_variable">Target Variable:</label>
@@ -28,29 +28,33 @@
                        :options="intervals"
                       @change="handle_select_interval">
         </b-form-select>
-        <label for = "model">Add Model:</label>
-      
-        <b-form-select name = "model"
-                      
-                      :options="models_to_add"
-                      @change="handle_select_model"
-                      width = "70%"
-                      >
-        </b-form-select>
       </form>
-      
       <br>
-     Current Models:
-      <div>
-     <ul class="list-group">
-       <li class="list-group-item" v-for="(item, index) in current_models" :key="index">
-    	  <span class="name">{{item}}</span>
-    		  <button class="btn btn-default btn-xs pull-right remove-item" @click="deleteFromList(item,index)" style="text-align: right">
-          <span class="glyphicon glyphicon-remove">❌</span>
-        </button>
-      </li>
-    </ul>
-  </div>
+      <label for = "data">Select Data:</label>
+      <div class="form-group form-check" v-for="(item, index) in data1" v-bind:key="index" >
+        <div v-if="index ==0">
+          <input type="checkbox" :id="item" :value="item" checked @click="handle_data(item,index)">&nbsp; {{item}}&nbsp;<span class="dot" style="background-color: lightgrey;"></span>
+        </div>
+        <div v-else>
+          <input type="checkbox" :id="item" :value="item" checked @click="handle_data(item,index)">&nbsp; {{item}}&nbsp;<span class="dot" style="background-color: black;"></span>
+        </div>
+      </div>
+      <label for = "model">Select Models:</label>
+      <div style=" height: 150px; overflow-y: scroll;">
+        <div class="form-group form-check" v-for="(item, index) in models" v-bind:key="index" >
+          <div v-if="forecasts.hasOwnProperty(item)">
+            <div v-if="item==='COVIDhub-ensemble'">
+              <input type="checkbox" :id="item" :value="item" @click="handle_models(item,index)" checked>&nbsp; {{item}}&nbsp;<span class="dot" v-bind:style="{ backgroundColor: colours[index%10]}"></span>
+            </div>
+            <div  v-else>
+              <input type="checkbox" :id="item" :value="item" @click="handle_models(item,index)" >&nbsp; {{item}}&nbsp;<span class="dot" v-bind:style="{ backgroundColor: colours[index%10]}"></span>
+            </div>
+          </div>
+          <div v-else style="color: lightgrey">
+            <input type="checkbox" :id="item" :value="item" @click="handle_models(item,index)" disabled="disabled">&nbsp; {{item}}&nbsp;<span class="dot" v-bind:style="{ backgroundColor: colours[index%10]}"></span>
+          </div>
+        </div>
+      </div>
     </div>
     
     <div id="viz" class="col-md-9">
@@ -73,22 +77,11 @@ export default {
   // },
   data() {
     return {
+      data1:['Current Truth', 'Truth As Of'],
+      colours :['#0d0887', '#46039f', '#7201a8', '#9c179e', '#bd3786', '#d8576b', '#ed7953', '#fb9f3a', '#fdca26', '#f0f921'],
       selected_target_variable: 'case',
       selected_location: 'US',
-      selected_interval:'50%',
-      selected_model:'USC-SI_kJalpha',
-      plot_layout: {
-        autosize: true,
-        legend: {
-       "orientation": "h",
-        font: {
-          
-          size: 12,
-         
-        },
-        
-  }
-      },
+      selected_interval:'95%',
       plot_style: {
         width: "100%",
         height:"80vh"
@@ -106,15 +99,19 @@ export default {
     intervals () {
       return this.$store.getters.intervals
     },
-    models_to_add(){
-      return this.$store.getters.models_to_add
+    models(){
+      return this.$store.getters.models
       },
-    current_models(){
-      return this.$store.getters.current_models
+      forecasts(){
+      return this.$store.getters.forecasts
       },
     plot_data () {
       return this.$store.getters.plot_data
+    },
+    plot_layout(){
+      return this.$store.getters.plot_layout
     }
+    
   },
   methods: {
     keydown_handler: function(event) {
@@ -134,14 +131,29 @@ export default {
     handle_select_interval: function(val) {
       this.$store.commit('set_interval', val)
     },
-    handle_select_model: function(val) {
-      this.$store.commit('add_to_current_model', val)
-      this.$store.commit('remove_from_models_to_add', val)
+    handle_models(item,index) {
+      var checkbox = document.getElementById(item);
+      if (checkbox.checked != true)
+      {
+        this.$store.commit('remove_from_current_model', item)
+      }
+      else{
+         this.$store.commit('add_to_current_model', item)
+      }
+      
     },
-    deleteFromList(item,index) {
-      this.$store.commit('remove_from_current_model', index)
-      this.$store.commit('add_to_models_to_add', item)
+    handle_data(item,index) {
+      var checkbox = document.getElementById(item);
+      if (checkbox.checked != true)
+      {
+        this.$store.commit('remove_from_data', item)
+      }
+      else{
+         this.$store.commit('add_to_data', item)
+      }
+      
     }
+
     
   },
   mounted() {
@@ -164,6 +176,14 @@ body,
   
   margin: 0;
 }
+.dot {
+  height: 15px;
+  width: 15px;
+  border-radius: 50%;
+  border: 1px solid black;
+  display: inline-block;
+  
+}
 
 #vizualizations {
   
@@ -176,5 +196,21 @@ body,
   background: #fff;
   padding: 5px;
   
+}
+/* width */
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  box-shadow: inset 0 0 5px grey; 
+  border-radius: 10px;
+}
+ 
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: grey; 
+  border-radius: 5px;
 }
 </style>
--- store/index.js
+++ store/index.js
@@ -20,22 +20,26 @@ export const state = () => ({
     current_truth: current_truth,
     forecasts: forecasts,
     models: models,
-    current_models: Object.keys(forecasts).includes('COVIDhub-ensemble')? ['COVIDhub-ensemble'] : [Object.keys(forecasts)[0]],
+    current_models:  ['COVIDhub-ensemble'],
     models_to_add: Object.keys(forecasts).slice(1,),
     interval_level: 95,
-    colours :['yellow','wheat','tomato','red','pink','blue', 'green','silver','gray','maroon','purple','fuchsia','lime','olive','navy','teal','aqua','aquamarine','orange','blueviolet','brown','burlywood','cadetblue','darksalmon','darkseagreen','darkslateblue','darkslategrey','darkturquoise','deeppink','deepskyblue','dodgerblue','yellowgreen']
+    data:['Current Truth','Truth As Of'],
+    colours:['#0d0887', '#46039f', '#7201a8', '#9c179e', '#bd3786', '#d8576b', '#ed7953', '#fb9f3a', '#fdca26', '#f0f921'],
 })
 
 export const mutations = {
     set_target_var (state, new_target_var) {
         state.target_var = new_target_var
+        state.current_models = ['COVIDhub-ensemble']
         this.dispatch('fetch_current_truth')
         this.dispatch('fetch_as_of_truth')
         this.dispatch('fetch_forecasts')
+      
         
     },
     set_location (state, new_location) {
         state.location = new_location
+        state.current_models = ['COVIDhub-ensemble']
         this.dispatch('fetch_current_truth')
         this.dispatch('fetch_as_of_truth')
         this.dispatch('fetch_forecasts')
@@ -47,6 +51,7 @@ export const mutations = {
     },
     increment_as_of (state) {
         let as_of_index = state.available_as_ofs[state.target_var].indexOf(state.as_of_date)
+        state.current_models = state.current_models.length >0? state.current_models: ['COVIDhub-ensemble']
         if (as_of_index < state.available_as_ofs[state.target_var].length - 1) {
             state.as_of_date = state.available_as_ofs[state.target_var][as_of_index + 1]
         }
@@ -56,6 +61,7 @@ export const mutations = {
     },
     decrement_as_of (state) {
         let as_of_index = state.available_as_ofs[state.target_var].indexOf(state.as_of_date)
+        state.current_models = state.current_models.length >0? state.current_models: ['COVIDhub-ensemble']
         if (as_of_index > 0) {
             state.as_of_date = state.available_as_ofs[state.target_var][as_of_index - 1]
         }
@@ -64,34 +70,32 @@ export const mutations = {
         console.log(state.as_of_date)
     },
     set_current_truth (state, new_truth) {
+
         state.current_truth = new_truth
     },
     set_as_of_truth (state, new_truth) {
         state.as_of_truth = new_truth
     },
     set_forecasts (state, new_forecasts) {
-        state.forecasts = new_forecasts
+        state.forecasts = new_forecasts 
     },
-    set_models_to_add (state) {
-        state.models_to_add = Object.keys(state.forecasts).slice(1,)
-    },
-    set_current_models (state) {
-        state.current_models = Object.keys(state.forecasts).slice(0,1)
-    },
-    remove_from_current_model (state, index) {
+    remove_from_current_model (state, item) {
+        let index = state.current_models.indexOf(item)
         state.current_models.splice(index,1)
-    },
-    remove_from_models_to_add (state, val) {
-        let index = state.models_to_add.indexOf(val)
-        state.models_to_add.splice(index,1)
+        console.log(state.current_models)
     },
     add_to_current_model(state, val){
-        state.current_models.push(val)
-    },
-    add_to_models_to_add(state, item){
-        state.models_to_add.push(item)
-    }
         
+        state.current_models.push(val)
+        console.log(state.current_models)
+    },
+    add_to_data(state, val){
+        state.data.push(val)
+    },
+    remove_from_data (state, item) {
+        let index = state.data.indexOf(item)
+        state.data.splice(index,1)
+    },  
 }
 
 export const actions = {
@@ -120,8 +124,6 @@ export const actions = {
             let target_path = 'data/forecasts/' + state.target_var + '_' + state.location + '_' + state.as_of_date + '.json'
             const response = await axios.get(target_path)
             commit('set_forecasts', response.data)        
-            commit('set_models_to_add')
-            commit('set_current_models')
             
         } catch (error) {
             commit('set_forecasts', {})
@@ -143,11 +145,36 @@ export const getters = {
     models: state => {
         return state.models
     },
-    models_to_add: state => {
-        return state.models_to_add
+    colours: state => {
+        return state.colours
+    },
+    data: state => {
+        return state.data
     },
     current_models:state => {
         return state.current_models
+    },
+    forecasts:state => {
+        return state.forecasts
+    },
+    plot_layout:state => {
+        let variable  = state.target_var==='case'? 'Cases': state.target_var =='hosp'? 'Hospitalisations':'Deaths'
+        let variable2 = state.target_var==='case'? 'Incident weekly Cases': state.target_var =='hosp'? 'Incident daily hospitalizations':'Incident weekly Deaths'
+        let result = locations.filter(obj => {
+            return obj.value === state.location
+          })
+        let location = result[0].text
+        return {
+            autosize: true,
+            showlegend:false,
+            title: {text:'Forecasts of '+variable+' in '+location +' as of '+state.as_of_date},
+            xaxis: {
+            title: {text: 'Date'}
+            },
+            yaxis: {
+            title: {text: variable2}
+            }
+          }
     },
     plot_data: (state, getters) => {
         var pd = Object.keys(state.forecasts).map(
@@ -168,7 +195,7 @@ export const getters = {
                             y: model_forecasts["q0.5"],
                             type: "scatter",
                             name: model,
-                            line: {color: state.colours[index]},
+                            line: {color: state.colours[index%10]},
                         },
                         {
                             // interval forecast -- currently fixed at 50%
@@ -179,7 +206,7 @@ export const getters = {
                                 model_forecasts[lower_quantile],
                                 model_forecasts[upper_quantile].slice().reverse()),
                             fill: "toself",
-                            fillcolor: state.colours[index],
+                            fillcolor: state.colours[index%10],
                             opacity: 0.3,
                             line: {color: "transparent"},
                             type: "scatter",
@@ -200,7 +227,7 @@ export const getters = {
                             y: model_forecasts["q0.5"],
                             type: "scatter",
                             name: model,
-                            line: {color: state.colours[index]},
+                            line: {color: state.colours[index%10]},
                         },
                         {
                             // interval forecast -- currently fixed at 50%
@@ -211,7 +238,7 @@ export const getters = {
                                 model_forecasts[lower_quantile],
                                 model_forecasts[upper_quantile].slice().reverse()),
                             fill: "toself",
-                            fillcolor: state.colours[index],
+                            fillcolor: state.colours[index%10],
                             opacity: 0.3,
                             line: {color: "transparent"},
                             type: "scatter",
@@ -229,7 +256,7 @@ export const getters = {
                         y: model_forecasts["q0.5"],
                         type: "scatter",
                         name: model,
-                        line: {color: state.colours[index]}
+                        line: {color: state.colours[index%10]}
                     }
                 }
                 }
@@ -242,6 +269,7 @@ export const getters = {
         )
         pd = [].concat(...pd)
         console.log(pd)
+        if(state.data.includes('Current Truth')){
         pd.push({
             x: state.current_truth.date,
             y: state.current_truth.y,
@@ -249,11 +277,11 @@ export const getters = {
             mode: "lines",
             name: "Current Truth",
             marker: {
-                color: "black"
+                color: "lightgray"
             }
         })
-        
-        if (state.as_of_date != state.current_date) {
+    }
+        if (state.as_of_date != state.current_date && state.data.includes('Truth As Of')) {
             pd.push({
                 x: state.as_of_truth.date,
                 y: state.as_of_truth.y,
@@ -261,7 +289,7 @@ export const getters = {
                 mode: "lines",
                 name: "Truth as of " + state.as_of_date,
                 marker: {
-                    color: "lightgray"
+                    color: "black"
                 }
             })
         }
