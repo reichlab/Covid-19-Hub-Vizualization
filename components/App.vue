ac2132a939a50b349fd06893a66de19f462e922b
vrushti-mody <vrushtimody6@gmail.com>
2021-10-07 21:24:06 -0400

    Statcounter

--- components/App.vue
+++ components/App.vue
@@ -144,12 +144,6 @@ var sc_security="3c7ad044";
 <script type="text/javascript"
 src="https://www.statcounter.com/counter/counter.js"
 async></script>
-<noscript><div class="statcounter"><a title="Web Analytics
-Made Easy - Statcounter" href="https://statcounter.com/"
-target="_blank"><img class="statcounter"
-src="https://c.statcounter.com/12239903/0/3c7ad044/1/"
-alt="Web Analytics Made Easy - Statcounter"
-referrerPolicy="no-referrer-when-downgrade"></a></div></noscript>
 <!-- End of Statcounter Code -->
 <script>
 // import { Plotly } from 'vue-plotly'
