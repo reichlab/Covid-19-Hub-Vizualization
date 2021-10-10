6fb9d54cf421326ab1c84792cceac1fe51359d64
vrushti-mody <vrushtimody6@gmail.com>
2021-10-10 03:55:02 -0400

    Fixed bugs

--- assets/analytics.js
+++ assets/analytics.js
@@ -5,5 +5,5 @@
 global.sc_project = 12239903
 global.sc_invisible = 1
 global.sc_security = "3c7ad044"
-global.scJsHost = (("https:" == document.location.protocol) ? "https://secure." : "http://www.")
+global.scJsHost = (("https:" == document.location.protocol) ? "https://viz." : "http://www.")
 document.write("<script type='text/javascript' src='" + scJsHost + "statcounter.com/counter/counter.js'></script>")
