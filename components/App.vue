c67fbacf217b945bca910c822e3398d92e4e1ef5
Evan Ray <elray@umass.edu>
2021-12-09 12:23:48 -0500

    initial updates moving viz functionality to an external module

--- components/App.vue
+++ components/App.vue
@@ -1,27 +1,21 @@
 <template>
   <div id="app">
     <my-header/>
-    <my-viz/>
+    <forecastViz/>
   </div>
-  
-
 </template>
+
 <script src="../assets/analytics.js">
 </script>
-
-
-
 
 <script>
 // import { Plotly } from 'vue-plotly'
 // import embed from vega-embed
 import Header from './Header'
-import Viz from './Viz.vue'
 export default {
-  components:{
-         'my-header':Header,
-         'my-viz':Viz
-     }
+  components: {
+    'my-header':Header
+  }
 }
 </script>
 
