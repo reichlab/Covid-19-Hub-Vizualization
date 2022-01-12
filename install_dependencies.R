e28d5845dd98367a2761f96a7e3b52d71d3a3918
Evan Ray <elray1@users.noreply.github.com>
2022-01-12 16:45:20 -0500

    update install_dependencies.R, remove covidData.

--- install_dependencies.R
+++ install_dependencies.R
@@ -1,4 +1,3 @@
 install.packages('devtools')
-devtools::install_github('reichlab/covidData')
 devtools::install_github('reichlab/covidHubUtils')
 install.packages(c('tidyverse', 'jsonlite', 'here'))
