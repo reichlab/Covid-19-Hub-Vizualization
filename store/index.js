127623ec6c11545c700168ca11e7dc13377b0879
Vrushti Mody <vrushtimody6@gmail.com>
2022-04-27 14:39:42 -0400

    Issue 40

--- store/index.js
+++ store/index.js
@@ -1,5 +1,9 @@
 // store/index.js
 import axios from 'axios';
+import target_variables from '../assets/target_variables.json';
+import locations from '../assets/locations.json';
+import available_as_ofs from '../static/data/available_as_ofs.json';
+import models from '../static/data/models.json';
 
 export const actions = {
   // fetch data by reading from a json file
@@ -22,5 +26,23 @@ export const actions = {
       data = response.data;
     }
     return data;
+  },
+
+  async forecastViz_options(_) {
+    return {
+        target_variables: target_variables,
+        init_target_var: 'death',
+        locations: locations,
+        init_location: 'US',
+        intervals: ['0%', '50%', '95%'],
+        init_interval: '95%',
+        available_as_ofs: available_as_ofs,
+        init_as_of_date: available_as_ofs.death[available_as_ofs.death.length - 1],
+        current_date: available_as_ofs.death[available_as_ofs.death.length - 1],
+        models: models,
+        default_models: ['COVIDhub-ensemble'],
+        all_models: false,
+        disclaimer: 'Most forecasts have failed to reliably predict rapid changes in the trends of reported cases and hospitalizations. Due to this limitation, they should not be relied upon for decisions about the possibility or timing of rapid changes in trends.'
+      }
   }
 }
